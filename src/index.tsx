ecb29b0e54543f326a681b1917a5e145bc791e57
JinJeon <nanyong01@gmail.com>
2022-05-24 12:26:43 +0900

    init : msw 설치 및 test api 생성

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "App";
+import worker from "MockData/browsers";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+if (process.env.NODE_ENV === "development") {
+	worker.start();
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
