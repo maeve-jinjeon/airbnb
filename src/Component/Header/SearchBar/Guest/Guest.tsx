b0a980b40e0b942631fd4c26531f9b7421379b34
Serin-Kim <serin9864@gmail.com>
2022-05-26 17:08:43 +0900

    feat : SearchBar 게스트 영역 상태 표시 추가
    Co-authored-by: JinJeon <JinJeon@users.noreply.github.com>

--- src/Component/Header/SearchBar/Guest/Guest.tsx
+++ src/Component/Header/SearchBar/Guest/Guest.tsx
@@ -1,18 +1,26 @@
+import { useContext } from "react";
+import { GuestsContext } from "Context/GuestsContext";
 import { CancelButton } from "util/Icons";
 import StyledGuest from "./Guest.styled";
 import { StyledSearchBarChild } from "../SearchBar.styled";
 
 const Guest = () => {
-	const name: string = "guest";
+	const name = "guest";
+	const { adult, child, baby } = useContext(GuestsContext);
+	const sum = adult + child + baby;
+	const mention = `게스트 ${adult + child}명, 유아 ${baby}명`;
+
 	return (
 		<StyledGuest>
 			<StyledSearchBarChild name={name}>
 				<div>인원</div>
-				<div>게스트 추가</div>
+				<div>{sum ? mention : "게스트 추가"}</div>
 			</StyledSearchBarChild>
 			<CancelButton colorset="grey3" size={20} />
 		</StyledGuest>
 	);
 };
+
+// 0 명일 때 게스트 추가 / 게스트 adult + child 명, 유아 baby 명
 
 export default Guest;
