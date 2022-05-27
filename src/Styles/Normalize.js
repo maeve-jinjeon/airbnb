26177e63d2d78d75bd250b449b2555f13671e522
JinJeon <nanyong01@gmail.com>
2022-05-27 12:41:27 +0900

    feat : modal 애니메이션 추가

--- src/Styles/Normalize.js
+++ src/Styles/Normalize.js
@@ -8,6 +8,24 @@ const Normalize = createGlobalStyle`
 			width: 100wh;
 		}
 	`}
+
+	@keyframes fade-in {
+			from {
+				opacity: 0;
+			}
+			to {
+				opacity: 1;
+			}
+		}
+
+	@keyframes fade-out {
+		from {
+			opacity: 1;
+		}
+		to {
+			opacity: 0;
+		}
+	}
 `;
 
 export default Normalize;
