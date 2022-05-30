7af8bb31f796c2092fe8459d13d11b5924a9daa9
JinJeon <nanyong01@gmail.com>
2022-05-31 02:09:01 +0900

    feat : hotels 데이터 수정 및  api 통해 가격 리스트 전달

--- src/MockData/handlers.js
+++ src/MockData/handlers.js
@@ -1,12 +1,30 @@
 import { rest } from "msw";
 import hotelsData from "./data";
 
+const priceUnit = 50000;
+
+const changePricePerUnit = (price, unitForDevide) => {
+	const devidedPrice = price - (price % unitForDevide);
+	return devidedPrice;
+};
+
 const getHotelsPrices = (hotels) => {
 	const sum = hotels.reduce((prev, { price }) => prev + price, 0);
 	const min = hotels.reduce((prev, { price }) => (prev <= price ? prev : price));
 	const max = hotels.reduce((prev, { price }) => (prev >= price ? prev : price));
 	const avg = Math.round(sum / (hotels.length - 1) / 100) * 100;
-	return { min, max, avg };
+	const prices = {};
+
+	hotels.forEach(({ price }) => {
+		const changedPrice = changePricePerUnit(price, priceUnit);
+		if (price >= 1000000) {
+			prices[1000000] = prices[1000000] ? prices[1000000] + 1 : 1;
+			return;
+		}
+		prices[changedPrice] = prices[changedPrice] ? prices[changedPrice] + 1 : 1;
+	});
+
+	return { min, max, avg, prices };
 };
 
 const handlers = [
