b39020aef378eea4c22c1400680b344920d19932
JinJeon <nanyong01@gmail.com>
2022-05-30 15:08:11 +0900

    feat : search bar price part 클릭 이벤트 설정

--- src/Component/Header/SearchBar/Price/Price.tsx
+++ src/Component/Header/SearchBar/Price/Price.tsx
@@ -1,13 +1,17 @@
+import { useContext } from "react";
+
+import { CheckModalContext } from "Context/modalContext";
 import { CancelButton } from "util/Icons";
 import StyledPrice from "./Price.styled";
 import { StyledSearchBarChild } from "../SearchBar.styled";
 
 const Price = () => {
-	const name: string = "price";
+	const checkModal = useContext(CheckModalContext);
+	const modalState = "price";
 
 	return (
 		<StyledPrice>
-			<StyledSearchBarChild name={name}>
+			<StyledSearchBarChild onClick={() => checkModal(modalState)} name={modalState}>
 				<div>요금</div>
 				<div>금액대 설정</div>
 			</StyledSearchBarChild>
