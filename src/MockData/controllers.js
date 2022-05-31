d0b49443e755958984906ae9e514fbf66f838d58
JinJeon <nanyong01@gmail.com>
2022-05-31 17:52:43 +0900

    refactor : hotels data 정리 함수 분리

---
+++ src/MockData/controllers.js
@@ -0,0 +1,27 @@
+const priceUnit = 50000;
+
+const changePricePerUnit = (price, unitForDevide) => {
+	const devidedPrice = price - (price % unitForDevide) + unitForDevide;
+	return devidedPrice;
+};
+
+const getHotelsPrices = (hotels) => {
+	const sum = hotels.reduce((prev, { price }) => prev + price, 0);
+	const min = hotels.reduce((prev, { price }) => (prev <= price ? prev : price));
+	const max = hotels.reduce((prev, { price }) => (prev >= price ? prev : price));
+	const avg = Math.round(sum / (hotels.length - 1) / 100) * 100;
+	const prices = {};
+
+	hotels.forEach(({ price }) => {
+		const changedPrice = changePricePerUnit(price, priceUnit);
+		if (price >= 1000000) {
+			prices[1000000] = prices[1000000] ? prices[1000000] + 1 : 1;
+			return;
+		}
+		prices[changedPrice] = prices[changedPrice] ? prices[changedPrice] + 1 : 1;
+	});
+
+	return { min, max, avg, prices };
+};
+
+export default getHotelsPrices;
--- src/MockData/handlers.js
+++ src/MockData/handlers.js
@@ -1,31 +1,6 @@
 import { rest } from "msw";
 import hotelsData from "./data";
-
-const priceUnit = 50000;
-
-const changePricePerUnit = (price, unitForDevide) => {
-	const devidedPrice = price - (price % unitForDevide) + unitForDevide;
-	return devidedPrice;
-};
-
-const getHotelsPrices = (hotels) => {
-	const sum = hotels.reduce((prev, { price }) => prev + price, 0);
-	const min = hotels.reduce((prev, { price }) => (prev <= price ? prev : price));
-	const max = hotels.reduce((prev, { price }) => (prev >= price ? prev : price));
-	const avg = Math.round(sum / (hotels.length - 1) / 100) * 100;
-	const prices = {};
-
-	hotels.forEach(({ price }) => {
-		const changedPrice = changePricePerUnit(price, priceUnit);
-		if (price >= 1000000) {
-			prices[1000000] = prices[1000000] ? prices[1000000] + 1 : 1;
-			return;
-		}
-		prices[changedPrice] = prices[changedPrice] ? prices[changedPrice] + 1 : 1;
-	});
-
-	return { min, max, avg, prices };
-};
+import getHotelsPrices from "./controllers";
 
 const handlers = [
 	rest.get(`/api/prices`, (req, res, ctx) => {
