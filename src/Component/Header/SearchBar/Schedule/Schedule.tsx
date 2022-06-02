8152581af7afe7339ca0dff2e419997e43fa7718
Serin-Kim <serin9864@gmail.com>
2022-06-02 22:20:14 +0900

    feat : SearchBar에서 체크아웃 누를 시 모달 이벤트 추가

--- src/Component/Header/SearchBar/Schedule/Schedule.tsx
+++ src/Component/Header/SearchBar/Schedule/Schedule.tsx
@@ -5,18 +5,20 @@ import StyledSchedule from "./Schedule.styled";
 import { StyledSearchBarChild } from "../SearchBar.styled";
 
 const Schedule = () => {
-	const modalState = "schedule";
+	const modalStateCheckin = "checkin";
+	const modalStateCheckout = "checkout";
+	const name = "schedule";
 	const checkModal = useContext(CheckModalContext);
 	const { checkin, checkout } = useContext(ScheduleContext);
 	const scheduleDispatch = useContext(ScheduleDispatchContext);
 
 	return (
 		<StyledSchedule>
-			<StyledSearchBarChild onClick={() => checkModal(modalState)} name={modalState}>
+			<StyledSearchBarChild onClick={() => checkModal(modalStateCheckin)} name={name}>
 				<div>체크인</div>
 				<div>{checkin.year ? `${checkin.month}월${checkin.date}일` : "날짜 입력"}</div>
 			</StyledSearchBarChild>
-			<StyledSearchBarChild name={modalState}>
+			<StyledSearchBarChild onClick={() => checkModal(modalStateCheckout)} name={name}>
 				<div>체크아웃</div>
 				<div>{checkout.year ? `${checkout.month}월${checkout.date}일` : "날짜 입력"}</div>
 			</StyledSearchBarChild>
