76c481c8a5c13106972b22bf37dcaff7233ddc8a
Serin-Kim <serin9864@gmail.com>
2022-06-02 18:04:53 +0900

    feat : SearchBar에 체크인&체크아웃 상태 표시, CancelButton 클릭 시 초기화

--- src/Component/Header/SearchBar/Schedule/Schedule.tsx
+++ src/Component/Header/SearchBar/Schedule/Schedule.tsx
@@ -1,24 +1,31 @@
 import { useContext } from "react";
 import { CancelButton } from "util/Icons";
-import { CheckModalContext } from "Context";
+import { CheckModalContext, ScheduleContext, ScheduleDispatchContext } from "Context";
 import StyledSchedule from "./Schedule.styled";
 import { StyledSearchBarChild } from "../SearchBar.styled";
 
 const Schedule = () => {
 	const modalState = "schedule";
 	const checkModal = useContext(CheckModalContext);
+	const { checkin, checkout } = useContext(ScheduleContext);
+	const scheduleDispatch = useContext(ScheduleDispatchContext);
 
 	return (
 		<StyledSchedule>
 			<StyledSearchBarChild onClick={() => checkModal(modalState)} name={modalState}>
 				<div>체크인</div>
-				<div>날짜 입력</div>
+				<div>{checkin.year ? `${checkin.month}월${checkin.date}일` : "날짜 입력"}</div>
 			</StyledSearchBarChild>
 			<StyledSearchBarChild name={modalState}>
 				<div>체크아웃</div>
-				<div>날짜 입력</div>
+				<div>{checkout.year ? `${checkout.month}월${checkout.date}일` : "날짜 입력"}</div>
 			</StyledSearchBarChild>
-			<CancelButton colorset="grey3" size={20} />
+			<CancelButton
+				colorset="grey3"
+				size={20}
+				onClick={() => scheduleDispatch({ dayInfo: { year: 0, month: 0, date: 0 }, type: "RESET" })}
+				hover="true"
+			/>
 		</StyledSchedule>
 	);
 };
