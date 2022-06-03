dad97a0be80509dd3e371cc4b41a8a9cbcf4e36f
JinJeon <nanyong01@gmail.com>
2022-06-03 11:05:53 +0900

    fix : error 수정
    Co-authored-by: Serin Kim  <Serin-Kim@users.noreply.github.com>

--- src/Component/Header/Modal/Modal.tsx
+++ src/Component/Header/Modal/Modal.tsx
@@ -2,13 +2,8 @@ import { useContext } from "react";
 import { ModalContext } from "Context";
 import { StyledModal, StyledModalWrapper } from "./Modal.styled";
 import GuestsDetail from "./GuestsDetail";
-<<<<<<< HEAD
 import PriceDetail from "./PriceDetail/PriceDetail";
 import CalendarDetail from "./CalendarDetail/CalendarDetail";
-=======
-import PriceDetail from "./PriceDetail";
-import CalendarDetail from "./CalendarDetail";
->>>>>>> 29b91d6 (feat : Detail 영역 생성 (#5))
 
 const Modal = () => {
 	const modal = useContext(ModalContext);
