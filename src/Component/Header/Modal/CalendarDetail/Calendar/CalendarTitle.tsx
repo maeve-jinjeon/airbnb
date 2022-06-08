52a4561e58d33a9b9184025627925033f58cb9cc
JinJeon <nanyong01@gmail.com>
2022-06-08 12:07:53 +0900

    refactor : 연도, 월 표시 부분 리펙토링

--- src/Component/Header/Modal/CalendarDetail/Calendar/CalendarTitle.tsx
+++ src/Component/Header/Modal/CalendarDetail/Calendar/CalendarTitle.tsx
@@ -1,12 +1,10 @@
 import { StyledCalendarTitle } from "./Calendar.styled";
 
 const CalendarTitle = ({ year, month }: { year: number; month: number }) => {
-	const showedMonth = month > 12 ? 1 : month + 1;
-	return (
-		<StyledCalendarTitle>
-			{year}년 {showedMonth}월
-		</StyledCalendarTitle>
-	);
+	const showedMonth = month >= 12 ? 1 : month + 1;
+	const title = `${year}년 ${showedMonth}월`;
+
+	return <StyledCalendarTitle>{title}</StyledCalendarTitle>;
 };
 
 export default CalendarTitle;
